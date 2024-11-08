f740bcf4d53e399897a804539641551a5ac605ab
Nihadname <nihadcoding@gmail.com>
2024-11-08 21:14:32 +0400

    created first Imagration of app

--- Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
+++ Persistence/obj/Debug/net8.0/Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04c838623846c2f7f31345eff6afc00689a40f1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bb2c1a756487331d98ac20c4fb4a82342b6a95")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
